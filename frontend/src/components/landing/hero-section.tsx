3bfc08eab92a59481fdccee4bca752f56182d2c3
WojCzu <wojciech.czuma@gmail.com>
2024-10-20 14:56:48 +0200

    feat: add common button

--- frontend/src/components/landing/hero-section.tsx
+++ frontend/src/components/landing/hero-section.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/common/Button";
 import { HeroImage } from "@/components/landing/hero-image";
 
 export function HeroSection() {
@@ -10,7 +11,9 @@ export function HeroSection() {
 						<h1 className="text-9xl font-extrabold">Craft Your Perfect Training Formula</h1>
 						<p className="text-4xl">Comprehensive Training Logs, Advanced Tools, Expert Insights</p>
 					</hgroup>
-					<button className="text-xl">Sign Up Now</button>
+					<Button size={"lg"} variant={"cta"}>
+						Sign Up Now
+					</Button>
 				</div>
 			</div>
 		</div>
