41b92cc2a9b253fe022de2c6a712434b5a787cb6
WojCzu <wojciech.czuma@gmail.com>
2024-10-26 00:09:26 +0200

    feat: add responsivness to hero section

--- frontend/src/components/landing/hero-section.tsx
+++ frontend/src/components/landing/hero-section.tsx
@@ -3,19 +3,23 @@ import { HeroImage } from "@/components/landing/hero-image";
 
 export function HeroSection() {
 	return (
-		<div className="h-dvh">
+		<section className="h-dvh px-8">
 			<HeroImage />
-			<div className="h-full px-10">
+			<div className="h-full">
 				<div className="mx-auto flex h-full max-w-[1800px] flex-col items-start justify-center gap-8">
 					<hgroup className="flex max-w-5xl flex-col gap-8">
-						<h1 className="text-9xl font-extrabold">Craft Your Perfect Training Formula</h1>
-						<p className="text-4xl">Comprehensive Training Logs, Advanced Tools, Expert Insights</p>
+						<h1 className="text-4xl font-extrabold md:text-6xl lg:text-9xl">
+							Craft Your Perfect Training Formula
+						</h1>
+						<p className="text-2xl md:text-3xl lg:text-4xl">
+							Comprehensive Training Logs, Advanced Tools, Expert Insights
+						</p>
 					</hgroup>
 					<Button size={"lg"} variant={"cta"}>
 						Sign Up Now
 					</Button>
 				</div>
 			</div>
-		</div>
+		</section>
 	);
 }
