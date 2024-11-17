2d42a16a83571e5d3b540ef56a0e5149decdb55f
WojCzu <wojciech.czuma@gmail.com>
2024-11-17 16:10:52 +0100

    feat: enhance accessibility and improve card component titles
    - Added `aria-labelledby` and `id` attributes to all sections and h2 elements for better accessibility.
    - Updated card component to render different headings based on the context

--- frontend/src/components/landing/call-to-action-section.tsx
+++ frontend/src/components/landing/call-to-action-section.tsx
@@ -2,8 +2,10 @@ import { Button } from "@/components/common/Button";
 
 export function CallToActionSection() {
 	return (
-		<section className="section-spacing container">
-			<h2 className="section-title">Ready to transform your training?</h2>
+		<section className="section-spacing container" aria-labelledby="call-to-action">
+			<h2 className="section-title" id="call-to-action">
+				Ready to transform your training?
+			</h2>
 			<div className="flex">
 				<Button size={"lg"} variant={"cta"} className="mx-auto">
 					Sign Up Now
